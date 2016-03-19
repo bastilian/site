46d2918302f988713bdf5aecca9bc130fd8882eb
Sebastian Gräßl <sebastian@validcode.me>
2016-03-19 12:40:50 +0100

    Improve map

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -7,14 +7,13 @@ function Map () {
     this.placesLayer   = new L.LayerGroup();
     this.marker        = L.divIcon({className: 'marker', iconSize : 5});
     this.currentMarker = L.divIcon({className: 'marker current', iconSize : 5});
+    this.map = L.map('map', { zoomControl: false, layers: [this.placesLayer]}).setView([55, -45], 3);
 
     this.setupMap();
     this.addMarkers();
   }
 
   this.setupMap = function () {
-    this.map = L.map('map', { zoomControl: false, layers: [this.placesLayer]}).setView([50, -14], 3);
-
     this.map.dragging.disable();
     this.map.touchZoom.disable();
     this.map.doubleClickZoom.disable();
@@ -27,7 +26,7 @@ function Map () {
   }
 
   this.mapAddress = function (address) {
-    geocoder.geocode({'address': address}, function(results, status) {
+    this.geocoder.geocode({'address': address}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
         console.log(address, [results[0].geometry.location.lat(), results[0].geometry.location.lng()]);
         L.marker([results[0].geometry.location.lat(),
@@ -40,21 +39,21 @@ function Map () {
   this.addMarkers = function () {
     this.places.forEach( function (city) {
       if (typeof city.latlng != 'undefined'){
-        console.log(city.location);
         var marker = L.marker(city.latlng, { icon: (city.current ? this.currentMarker : this.marker) } ).addTo(this.placesLayer)
 
         if (city.current) {
           marker.bindPopup("currently living in<br/> <strong>" + city.location + "</strong>", { closeButton: false, zoomAnimation: false, className: 'tooltip'}).openPopup()
         }
       } else {
-        this.mapAddress(city)
+        this.mapAddress(city.location)
       }
     }.bind(this));
   }
 
   this.loadTileLayer = function () {
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + this.token, {
-      id: 'mapbox.light'
+      id: 'mapbox.light',
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
     }).addTo(this.map)
   }
 
